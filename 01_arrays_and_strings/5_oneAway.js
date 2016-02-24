717f3030f594a11fedc4441c9a1f71b3ecab8878
Joshua Ho <ho.joshua4@gmail.com>
2016-02-23 18:06:12 -0800

    1.5 Done

---
+++ 01_arrays_and_strings/5_oneAway.js
@@ -0,0 +1,50 @@
+function isOneAway (string1, string2) {
+
+  var count = 0;
+  var long, short;
+  var diff = 1;
+
+  if (string1.length > string2.length) {
+    long = string1;
+    short = string2;
+  } else {
+    long = string2;
+    short = string1;
+  }
+  for (var i = 0; i < long.length; i++) {
+    if(long[i] !== short[i]) count += 1;
+  };
+  if (count > diff) return false;
+  return true;
+};
+
+//Test for A-A-Ron
+console.log(isOneAway('regex' , 'regexepleslkjfasldkjfasdfsd'));
+console.log('Should be false');
+console.log(isOneAway('bear' , 'b'));
+console.log('Should be false');
+
+//Test for De-Nice
+console.log( isOneAway('bear' , 'pear') );
+console.log('Should be true');
+console.log(isOneAway('bear' , 'bea'));
+console.log('Should be true');
+
+//Written by Josh
+
+//NOTE: Longest string is being calculated in case the questions asks us to calculate
+// differences larger than 1 (like 10). This is needed to prevent a length
+//difference and a character difference from being counted twice.
+
+
+
+
+
+
+
+
+
+
+
+
+//Altered to calculate all changes past 1
