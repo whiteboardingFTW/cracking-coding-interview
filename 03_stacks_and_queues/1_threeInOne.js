5d3d079f187241328da69c557aeb1313f29697f1
SABRINA TEE <sabrinatee@me.com>
2016-03-05 17:27:25 -0800

    Added method to 3.1

--- 03_stacks_and_queues/1_threeInOne.js
+++ 03_stacks_and_queues/1_threeInOne.js
@@ -41,4 +41,67 @@ test.push('b1', 1);
 test.push('b2', 1);
 test.push('c', 2);
 
-debugger;
+// Method #2
+var ThreeInOne2 = function() {
+  var stack1 = [];
+  var stack2 = [];
+  var stack3 = [];
+
+  this.push = function(val) {
+    if(!val) return null;
+    var pointer = ((stack1.length + stack2.length + stack3.length) / 3).toFixed(1);
+    var split = pointer.toString().split('.')[1];
+    if (split == 0) stack1.push(val);
+    else if (split == 3) stack2.push(val);
+    else if (split == 7) stack3.push(val);
+  };
+
+  this.pop = function() {
+    var pointer = ((stack1.length + stack2.length + stack3.length) / 3).toFixed(1);
+    var split = pointer.toString().split('.')[1];
+    if (!stack1.length && !stack2.length && !stack3.length) return console.log('All stacks empty');
+    else if (split == 0) stack3.pop();
+    else if (split == 7) stack2.pop();
+    else if (split == 3) stack1.pop();
+  };
+
+  this.display = function() {
+    console.log('stack 1 ' + stack1);
+    console.log('stack 2 ' + stack2);
+    console.log('stack 3 ' + stack3);
+  };
+};
+
+var bob = new ThreeInOne2();
+bob.push('a1');
+bob.display();
+bob.push('a2');
+bob.display();
+bob.push('a3');
+bob.display();
+bob.push('b1');
+bob.display();
+bob.push('b2');
+bob.display();
+bob.push('c1');
+bob.display();
+bob.pop();
+bob.display();
+bob.pop();
+bob.display();
+bob.pop();
+bob.display();
+bob.pop();
+bob.display();
+bob.pop();
+bob.display();
+bob.pop();
+bob.display();
+
+var cheese = new ThreeInOne2();
+cheese.pop();
+cheese.push('brie');
+cheese.display();
+cheese.pop();
+cheese.display();
+cheese.pop();
